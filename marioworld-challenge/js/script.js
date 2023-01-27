a14888acbce211c8c13cd14807754eb9c67656fb
Luiz Antunes <luizrantunes@outlook.com>
2023-01-26 23:20:19 -0300

    updates

--- marioworld-challenge/js/script.js
+++ marioworld-challenge/js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -7,8 +8,32 @@ const jump = () => {
 
         mario.classList.remove('jump');
 
-    }, 500)
-
+    }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition)
+    
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 90) {
+
+        pipe.style.animation ='none';
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation ='none';
+        mario.style.bottom = `${marioPosition }px`
+
+        mario.src='./images/gameover.png';
+        mario.style.width='75px'
+        mario.style.marginLeft ='50px'
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);

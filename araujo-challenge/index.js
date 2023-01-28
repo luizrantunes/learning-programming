409ecff60cd6bbc404609ab51d88a41679e99798
Luiz Antunes <luizrantunes@outlook.com>
2023-01-27 23:15:26 -0300

    code update

--- araujo-challenge/index.js
+++ araujo-challenge/index.js
@@ -1,5 +1,5 @@
 
-function combinacaoLetra(str) {
+/*function combinacaoLetra(str) {
     let saida = ""
     let indice = 0
     for (indice; indice < str.length; indice++) {
@@ -13,4 +13,25 @@ function combinacaoLetra(str) {
     console.log(str)
 }
 
-combinacaoLetra("ABC")
+combinacaoLetra("ABC") */
+
+// Código acima inválido como resposta do enunciado
+
+function combinacaoLetra(str) {
+    let binario = str.length;
+    let indice = 0
+    for (indice; indice < (1 << binario); indice++) {
+        let saida = "";
+        let auxiliar = 0
+        for (auxiliar; auxiliar < binario; auxiliar++) {
+            if (indice & (1 << auxiliar)) {
+                saida += str[auxiliar];
+            }
+        }
+        if (saida.length > 0) {
+            console.log(saida);
+        }
+    }
+}
+
+combinacaoLetra("MOVEL");
